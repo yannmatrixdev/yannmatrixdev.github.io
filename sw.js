c8005f73e6ab0fc3ae8fffbce11442be164d1832
Decode-Chan <70747479+decodexuwu@users.noreply.github.com>
2022-09-09 13:46:06 -0400

    Updated Scope

--- sw.js
+++ sw.js
@@ -3,24 +3,18 @@ self.addEventListener("install", e => {
         caches.open("static").then(cache => {
             return cache.addAll([
                 "./",
-                "./about.html",
-                "./login.html",
-                "./404.html",
-                "./projects.html",
-                "./css/main.css",
-                "./css/font.css",
-                "./css/themes/decode.css",
-                "./css/themes/default.css",
-                "./css/themes/josin.css",
-                "./css/themes/koawa.css",
-                "./css/themes/light.css",
-                "./css/themes/scorpion.css",
-                "./css/themes/scorpion-sh.css",
-                "./css/themes/yannmatrix.css",
-                "./script/progressbar.js",
-                "./script/script.js",
-                "./script/themes.js",
-                "./audio/canweget.mp3"
+                "./src/css/main.css",
+                "./src/css/fonts/font0.ttf",
+                "./src/css/fonts/font1.ttf",
+                "./src/img/icon_01.png",
+                "./src/img/icon_02.png",
+                "./src/img/icon_03.png",
+                "./src/img/icon_out.png",
+                "./src/img/icon-1x.png",
+                "./src/img/icon-2x.png",
+                "./src/img/icon.png",
+                "./src/script/index.js",
+                "./src/script/worker.js"
             ])
         })
     )
